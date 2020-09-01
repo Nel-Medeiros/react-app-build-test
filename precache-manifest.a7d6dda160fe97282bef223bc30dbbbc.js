1589d0606630e3839788d59685ac5f1d32bd83c4
Nel-Medeiros <emanuelpaivamedeiros@gmail.com>
2020-09-01 12:44:58 -0300

    Updates

---
+++ precache-manifest.a7d6dda160fe97282bef223bc30dbbbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b83240be0375dde871713887141ecef",
+    "url": "/react-app-build-test/index.html"
+  },
+  {
+    "revision": "055862cfe89dbd5e3d1d",
+    "url": "/react-app-build-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb43c5dd6ae9c5788a7b",
+    "url": "/react-app-build-test/static/js/2.da3882de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-build-test/static/js/2.da3882de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "055862cfe89dbd5e3d1d",
+    "url": "/react-app-build-test/static/js/main.1dceff76.chunk.js"
+  },
+  {
+    "revision": "14ea772af7c8a79116bc",
+    "url": "/react-app-build-test/static/js/runtime-main.95b055cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-build-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
